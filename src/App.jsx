b8dbed260fe128a73697c34744c0ef3ac6ba88b2
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-19 16:26:10 -0800

    Add input field and fetch words based on input value

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
+import { useState, useEffect } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 function App() {
+  const [input, setInput] = useState('');
   const queryClient = useQueryClient();
-  const postsQuery = useQuery({
-    queryKey: ['words'],
-    queryFn: (obj) =>
-      wait(1000).then(() => {
-        console.log('hello');
-      }),
+  const { data: words, isLoading } = useQuery({
+    queryKey: ['words', input],
+    queryFn: () => fetchWords(input),
+    enabled: input.length > 0,
   });
+
+  useEffect(() => {
+    if (input.length > 0) {
+      queryClient.invalidateQueries(['words', input]);
+    }
+  }, [input, queryClient]);
 
   return (
     <>
-      <input type="text" />
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        words && (
+          <ul>
+            {words.map((word, index) => (
+              <li key={index}>{word.word}</li>
+            ))}
+          </ul>
+        )
+      )}
     </>
   );
 }
 
 function fetchWords(word) {
-  return fetch('');
+  return fetch(`https://api.datamuse.com/words?rel_syn=${word}`).then(
+    (response) => response.json()
+  );
 }
 
 export default App;
